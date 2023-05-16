90a16ae8c64177ee219abc0be9e313c10511bffa
Amelia Freeman <ameliakfreeman@gmail.com>
2023-05-15 21:27:32 -0500

    css updated

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const h4 = document.createElement("h4");
+h4.textContent = "This website created by Amelia Freeman";
+document.querySelector("body").appendChild(h4);
+
 
